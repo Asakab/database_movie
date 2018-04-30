3d32477d1f42edc1ebd8a2ef9577a7cb945c6788
Selma <asakab@live.fr>
2018-04-30 22:22:43 +0200

    lalala

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,19 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+
+
 Movie.destroy_all
+Director.destroy_all
 
-Movie.create(title: "Fight Club", release_year: 1999, director: "David Fisher")
-Movie.create(:title => " Full Metal Jacket", :release_year => 1987,  :director => "Kubrick")
-Movie.create(:title  => "Ip Man",:release_year => 2008, :director => "Wilson yip")
-Movie.create(:title => "Bullet in the head", :release_year => 1993, :director => "John Woo")
-Movie.create(:title => "Edward aux mains d'argent", :release_year => 1990, :director => "Tim Burton")
-
+a = Director.create(first_name: "David", lastname: "Fisher")
+Movie.create(title: "Fight Club", release_year: 1999, director_id: a.id)
+b = Director.create(first_name: "Stanley", lastname: "Kubrick")
+Movie.create(title: "Full Metal Jacket", release_year: 1987, director_id: b.id)
+c = Director.create(first_name: "Wilson", lastname: "Yip")
+Movie.create(title: "Ip Man", release_year: 2008, director_id: c.id)
+d = Director.create(first_name: "John", lastname: "Woo")
+Movie.create(title: "Bullet in the head", release_year: 1993, director_id: d.id)
+e = Director.create(first_name: "Tim", lastname: "Burton")
+Movie.create(title: "Edward aux mains d'argent", release_year: 1990, director_id: e.id)
